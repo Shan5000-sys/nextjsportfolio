8ae5be642df41d88a7f4595135a321a5ee038f98
Shangeeth Umasaran <shangeeth023@gmail.com>
2025-03-29 00:54:47 -0400

    Switch to static export for Amplify

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // <- this enables static export mode
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
 };
 
 export default nextConfig;
